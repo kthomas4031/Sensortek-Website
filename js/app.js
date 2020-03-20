1ba420b9c27ba4964ab4e4e35b62c503cfbe348c
Kyle Thomas <39631918+kthomas4031@users.noreply.github.com>
2020-03-20 16:26:16 -0400

    Added file upload capabilites

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 window.addEventListener('load', async () => {
     particlesJS.load('particles-js', './js/particles.json');    
 });
+
+document.querySelector('.file-select').addEventListener('change', handleFileUploadChange());
+document.querySelector('.file-submit').addEventListener('click', handleFileUploadSubmit());
+
+let selectedFile;
+function handleFileUploadChange(e){
+  selectedFile = e.target.files[0];
+}
+
+function handleFileUploadSubmit(e) {
+    //TODO: PARSE AND DISPLAY FILE DATA SO USER CAN CONFIRM BEFORE SENDING TO DATABASE
+
+    //TODO: PUSH DATA TO DATABASE
+  }
